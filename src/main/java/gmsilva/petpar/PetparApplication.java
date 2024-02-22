a530e4eea474495c7732967472f748ae07589a31
guimoll <109749936+fennec001@users.noreply.github.com>
2024-02-22 00:23:51 -0300

    Criacao do projeto

---
+++ src/main/java/gmsilva/petpar/PetparApplication.java
@@ -0,0 +1,13 @@
+package gmsilva.petpar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetparApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetparApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gmsilva/petpar/PetparApplicationTests.java
@@ -0,0 +1,13 @@
+package gmsilva.petpar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetparApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
